847e422fb496f87bb82c4c0028ad933b95ce2784
SchwartzLucas <lucassdesouza10@gmail.com>
2026-02-24 20:16:59 -0300

    first commit

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,27 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
+public class AnimeController {
+
+    private DateUtil dateUtil;
+    //localhost:8080/anime/list
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabseStyle(LocalDateTime.now()));
+        return List.of(new Anime("TESTE"), new Anime("Berserk"), new Anime("Bleach"));
+    }
+}
